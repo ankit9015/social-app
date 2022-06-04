9b3d5fdf8a691bc74d1c647d5b14ae0580d07bda
Ankit Joshi <67504902+ankit9015@users.noreply.github.com>
2022-06-04 15:11:19 +0530

    Revert "Feat  homepage"

--- src/features/common/index.js
+++
@@ -1,8 +0,0 @@
-import SearchBox from "./SearchBox/SearchBox";
-import AvatarInfo from "./AvatarInfo/AvatarInfo";
-import Modal from "./Modal/Modal";
-import FollowList from "./FollowList/FollowList";
-import PostEditor from "./PostEditor/PostEditor";
-import PostBox from "./PostBox/PostBox";
-
-export { SearchBox, AvatarInfo, Modal, FollowList, PostEditor, PostBox };
--- src/features/index.js
+++
@@ -1,5 +0,0 @@
-import HomePage from "./HomePage/HomePage";
-import Explore from "./Explore/Explore";
-import Bookmarks from "./Bookmarks/Bookmarks";
-
-export { HomePage, Explore, Bookmarks };
